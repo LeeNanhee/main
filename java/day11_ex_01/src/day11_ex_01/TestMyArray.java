4113adf1799744966461b6d9aca88049ccb68e02
LeeNanhee <153256917+LeeNanhee@users.noreply.github.com>
2023-12-15 18:01:27 +0900

    Add files via upload

---
+++ java/day11_ex_01/src/day11_ex_01/TestMyArray.java
@@ -0,0 +1,20 @@
+package day11_ex_01;
+
+public class TestMyArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MyArray array = new MyArray();
+		
+		array.add(10);
+		array.add(20);
+		array.add(30);
+		array.add(40);
+		array.insert(2,50);
+		
+		array.printAll();
+		
+	}
+
+}
